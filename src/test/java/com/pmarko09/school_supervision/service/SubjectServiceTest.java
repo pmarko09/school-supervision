e382421b31c990606a90c932b2c15dccf74dee60
pmarko <p.marko09@yahoo.com>
2024-10-01 15:59:00 +0200

    SubjectServiceTest missing methods added, StudentServiceTest and TeacherServiceTest added too

--- src/test/java/com/pmarko09/school_supervision/service/SubjectServiceTest.java
+++ src/test/java/com/pmarko09/school_supervision/service/SubjectServiceTest.java
@@ -2,18 +2,20 @@ package com.pmarko09.school_supervision.service;
 
 import com.pmarko09.school_supervision.mapper.SubjectMapper;
 import com.pmarko09.school_supervision.model.dto.SubjectDTO;
-import com.pmarko09.school_supervision.model.entity.Student;
-import com.pmarko09.school_supervision.model.entity.Subject;
-import com.pmarko09.school_supervision.model.entity.Teacher;
+import com.pmarko09.school_supervision.model.entity.*;
 import com.pmarko09.school_supervision.repository.ExamRepository;
 import com.pmarko09.school_supervision.repository.StudentRepository;
 import com.pmarko09.school_supervision.repository.SubjectRepository;
 import com.pmarko09.school_supervision.repository.TeacherRepository;
+import com.pmarko09.school_supervision.validation.ExamValidation;
+import com.pmarko09.school_supervision.validation.SubjectValidation;
+import com.pmarko09.school_supervision.validation.TeacherValidation;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mapstruct.factory.Mappers;
 import org.mockito.Mockito;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -21,6 +23,7 @@ import java.util.Set;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
 public class SubjectServiceTest {
@@ -88,6 +91,7 @@ public class SubjectServiceTest {
         //then
         assertEquals("Polski", result.getName());
         assertEquals(1L, result.getId());
+        assertEquals(subject.getTeacher().getId(), result.getTeacherId());
     }
 
     @Test
@@ -100,6 +104,7 @@ public class SubjectServiceTest {
         Student student = new Student();
         subject.setStudents(Set.of(student));
 
+        SubjectValidation.validateSubjectData(subject);
         when(subjectRepository.save(any())).thenReturn(subject);
         subjectMapper.toDto(subject);
 
@@ -113,6 +118,173 @@ public class SubjectServiceTest {
 
     @Test
     void updateSubject_DataCorrect_SubjectDtoReturned() {
+
+        //given
+        Long id = 5L;
+
+        Subject subject = new Subject();
+        subject.setId(5L);
+        subject.setName("Polski");
+        Student student = new Student();
+        subject.setStudents(Set.of(student));
+
+        Subject updatedSubject = new Subject();
+        updatedSubject.setId(5L);
+        updatedSubject.setName("ANG");
+        Student newStudent = new Student();
+        updatedSubject.setStudents(Set.of(newStudent));
+
+        when(subjectRepository.findById(id)).thenReturn(Optional.of(subject));
+        SubjectValidation.validateSubjectData(updatedSubject);
+        Subject.update(subject, updatedSubject);
+        when(subjectRepository.save(any())).thenReturn(subject);
+        SubjectDTO subjectDTO = subjectMapper.toDto(subject);
+
+        //when
+        SubjectDTO result = subjectService.updateSubject(id, updatedSubject);
+
+        //then
+        assertEquals(subjectDTO.getId(), result.getId());
+        assertEquals(subjectDTO.getName(), result.getName());
+        assertEquals(subjectDTO.getTeacherId(), result.getTeacherId());
+        assertEquals(subjectDTO.getStudentIds(), result.getStudentIds());
+    }
+
+    @Test
+    void deleteSubject_DataCorrect_SubjectDtoReturned() {
+
+        //given
+        Long id = 5L;
+
+        Subject subject = new Subject();
+        subject.setId(5L);
+        subject.setName("Polski");
+        Student student = new Student();
+        subject.setStudents(Set.of(student));
+
+        when(subjectRepository.findById(id)).thenReturn(Optional.of(subject));
+        doNothing().when(subjectRepository).delete(subject);
+        SubjectDTO subjectDTO = subjectMapper.toDto(subject);
+
+        //when
+        SubjectDTO result = subjectService.deleteSubject(id);
+
+        //then
+        assertEquals(5L, result.getId());
+        assertEquals("Polski", result.getName());
+        assertEquals(subjectDTO.getStudentIds(), result.getStudentIds());
+    }
+
+    @Test
+    void assignSubjectToTeacher_DataCorrect_SubjectDtoReturned() {
+
+        //given
+        Long subjectId = 5L;
+        Long teacherId = 3L;
+
+        Subject subject = new Subject();
+        subject.setId(5L);
+        subject.setName("Polski");
+        Student student = new Student();
+        subject.setStudents(Set.of(student));
+        subject.setTeacher(null);
+
+        Teacher teacher = new Teacher();
+        teacher.setId(3L);
+        teacher.setFirstname("Jan");
+        teacher.setLastname("Kowal");
+        teacher.setEmail("K@");
+
+        when(subjectRepository.findById(subjectId)).thenReturn(Optional.of(subject));
+        when(teacherRepository.findById(teacherId)).thenReturn(Optional.of(teacher));
+
+        SubjectValidation.validateSubjectData(subject);
+        TeacherValidation.validateTeacherData(teacher);
+
+        when(subjectRepository.save(any())).thenReturn(subject);
+
+        SubjectDTO subjectDTO = subjectMapper.toDto(subject);
+        subjectDTO.setTeacherId(teacher.getId());
+
+        //when
+        SubjectDTO result = subjectService.assignSubjectToTeacher(subjectId, teacherId);
+
+        //then
+        assertEquals("Polski", result.getName());
+        assertEquals(subjectDTO.getId(), result.getId());
+        assertEquals(subjectDTO.getTeacherId(), result.getTeacherId());
+    }
+
+    @Test
+    void addExamToSubject_DataCorrect_SubjectDtoReturned() {
+        //given
+        Long subjectId = 5L;
+        Long examId = 3L;
+
+        Subject subject = new Subject();
+        subject.setId(subjectId);
+        subject.setName("Polski");
+        Student student = new Student();
+        subject.setStudents(Set.of(student));
+
+        Exam exam = new Exam();
+        exam.setId(examId);
+        exam.setTime(LocalDateTime.now());
+        exam.setSubject(null);
+
+        when(subjectRepository.findById(subjectId)).thenReturn(Optional.of(subject));
+        when(examRepository.findById(examId)).thenReturn(Optional.of(exam));
+
+        ExamValidation.validateExamData(exam);
+        SubjectValidation.thisExamAlreadyAssigned(subject, exam);
+
+        when(subjectRepository.save(any())).thenReturn(subject);
+
+        SubjectDTO subjectDTO = subjectMapper.toDto(subject);
+        subjectDTO.setExamIds(Set.of(exam.getId()));
+
+        //when
+        SubjectDTO result = subjectService.addExamToSubject(subjectId, examId);
+
+        //then
+        assertEquals(5L, result.getId());
+        assertEquals("Polski", result.getName());
+        assertEquals(subjectDTO.getExamIds(), result.getExamIds());
+    }
+
+    @Test
+    void addStudentToSubject_DataCorrect_SubjectDtoReturned() {
+        //given
+        Long subjectId = 5L;
+        Long studentId = 3L;
+
+        Subject subject = new Subject();
+        subject.setId(subjectId);
+        subject.setName("Polski");
+
+        Student student = new Student();
+        student.setId(studentId);
+        student.setFirstname("JA");
+        student.setLastname("BE");
+        student.setEmail("AA@");
+
+        when(subjectRepository.findById(subjectId)).thenReturn(Optional.of(subject));
+        when(studentRepository.findById(studentId)).thenReturn(Optional.of(student));
+
+        SubjectValidation.thisStudentAlreadyAssigned(subject, student);
+
+        when(subjectRepository.save(any())).thenReturn(subject);
+
+        SubjectDTO subjectDTO = subjectMapper.toDto(subject);
+        subjectDTO.setStudentIds(Set.of(student.getId()));
+
+        //when
+        SubjectDTO result = subjectService.addStudentToSubject(subjectId, studentId);
+
+        //then
+        assertEquals(5L, result.getId());
+        assertEquals("Polski", result.getName());
+        assertEquals(subjectDTO.getStudentIds(), result.getStudentIds());
     }
 }
 
